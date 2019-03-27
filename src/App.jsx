66d1ee3d05b31e7160c8beb30eabb01f6dc298d7
Chrismarcel <cmarcelj@gmail.com>
2019-03-27 23:37:55 +0100

    chore(webpack): configure webpack
    - install webpack
    - install required webpack plugins
    - install omitted eslint and babel plugins
    - include entry points and test app.jsx script
    
    [Finishes #164010330]

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Home = () => {
+  return <div>Welcome to iReporter</div>;
+};
+
+ReactDOM.render(<Home />, document.getElementById("app"));
