fda39bf5a38561e8cd52b5ef9016ca6494873ce9
Chrismarcel <cmarcelj@gmail.com>
2019-03-28 14:21:24 +0100

    chore(webpack): set up sass preprocessor
    - update webpack config
    - install necessary dependencies to compile sass
    - include prettier config
    - include test sass file
    
    [Finishes #164893784]

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const htmlPlugin = new HtmlWebpackPlugin({
   template: './public/index.html',
   filename: './index.html'
-})
+});
+
+const cssPlugin = new MiniCssExtractPlugin({
+  filename: './assets/style.css'
+});
 
 module.exports = {
   entry: './src/App.jsx',
   devtool: 'cheap-eval-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/assets/'
+    filename: 'bundle.js'
   },
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.css']
@@ -27,8 +31,17 @@ module.exports = {
         exclude: /node_modules/,
         test: /\.jsx?$/,
         use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.(scss|css|sass)$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
       }
     ]
   },
-  plugins: [htmlPlugin]
-}
+  plugins: [cssPlugin, htmlPlugin]
+};
