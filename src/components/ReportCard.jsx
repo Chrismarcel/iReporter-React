a7b322319fd34055b46bdee021024bf942565931
Chrismarcel <cmarcelj@gmail.com>
2019-05-01 20:43:10 +0100

    bug: fix error when image is being loaded
    - ensure tests are passing
    - fix aforementioned error
    
    [Finishes #165752375]

--- src/components/ReportCard.jsx
+++ src/components/ReportCard.jsx
@@ -8,62 +8,65 @@ import HelperUtils from '../utils/HelperUtils';
  * @description - Card component for rendering reports
  * @returns {JSX} JSX markup
  */
-const ReportCard = ({ report, toggleModal }) => (
-  <div className="card report-card">
-    <p className="report-time">
-      <i className="icon icon-blue fas fa-clock" />
-      {HelperUtils.convertUTCTOLocalTime(report.createdat)}
-    </p>
-    <div className={`report-status ${report.status}`}>{report.status}</div>
-    <p data-type="red-flag" data-id="25" className="report-comment">
-      {report.comment}
-    </p>
-    <Link
-      to={`./aricles/${report.type}s/${report.id}`}
-      id={report.id}
-      onClick={toggleModal}
-      className="expand-report"
-      data-type="report"
-    >
-      See more details
-    </Link>
-    <div className="report-media">
-      <div className="report-image">
-        <i className="icon icon-blue fas fa-images" />
-        {report.images.length}
-        {' '}
-        {report.images.length === 1 ? 'Photo' : 'Photos'}
-      </div>
-    </div>
-    <p className="report-location">
-      <i className="icon icon-blue fas fa-map-marker-alt" />
-      {`${report.latitude}, `}
-      {report.longitude}
-    </p>
-    {report.status === 'drafted' && (
+const ReportCard = ({ report, toggleModal }) => {
+  const reportImages = report.images || [];
+  return (
+    <div className="card report-card">
+      <p className="report-time">
+        <i className="icon icon-blue fas fa-clock" />
+        {HelperUtils.convertUTCTOLocalTime(report.createdat)}
+      </p>
+      <div className={`report-status ${report.status}`}>{report.status}</div>
+      <p data-type="red-flag" data-id="25" className="report-comment">
+        {report.comment}
+      </p>
       <Link
+        to={`./aricles/${report.type}s/${report.id}`}
         id={report.id}
-        to={`./edit-report/${report.type}s/${report.id}`}
-        className="btn btn-primary edit-report"
-      >
-        <i className="icon icon-white fas fa-pen" />
-        Edit Report
-      </Link>
-    )}
-    {report.status === 'drafted' && (
-      <button
-        type="button"
-        className="btn btn-warning delete-report"
-        id={report.id}
-        data-type="delete"
         onClick={toggleModal}
+        className="expand-report"
+        data-type="report"
       >
-        <i className="icon icon-white fas fa-trash-alt" />
-        Delete Report
-      </button>
-    )}
-  </div>
-);
+        See more details
+      </Link>
+      <div className="report-media">
+        <div className="report-image">
+          <i className="icon icon-blue fas fa-images" />
+          {reportImages.length}
+          {' '}
+          {reportImages.length === 1 ? 'Photo' : 'Photos'}
+        </div>
+      </div>
+      <p className="report-location">
+        <i className="icon icon-blue fas fa-map-marker-alt" />
+        {`${report.latitude}, `}
+        {report.longitude}
+      </p>
+      {report.status === 'drafted' && (
+        <Link
+          id={report.id}
+          to={`./edit-report/${report.type}s/${report.id}`}
+          className="btn btn-primary edit-report"
+        >
+          <i className="icon icon-white fas fa-pen" />
+          Edit Report
+        </Link>
+      )}
+      {report.status === 'drafted' && (
+        <button
+          type="button"
+          className="btn btn-warning delete-report"
+          id={report.id}
+          data-type="delete"
+          onClick={toggleModal}
+        >
+          <i className="icon icon-white fas fa-trash-alt" />
+          Delete Report
+        </button>
+      )}
+    </div>
+  );
+};
 
 /**
  * @method EmptyCard
